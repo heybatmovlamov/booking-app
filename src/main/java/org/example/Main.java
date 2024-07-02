d62f12ee4f7f8d47d19e393b39e578784ed836c1
Heybat <heybetmovlamov2005@gmail.com>
2024-07-02 10:17:34 +0100

    Server config update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import lombok.SneakyThrows;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.example.controller.flightServlet.FlightByIdServlet;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    @SneakyThrows
+    public static void main(String[] args) {
+        Server server = new Server(9000);
+
+        ServletContextHandler handler = new ServletContextHandler();
+
+        handler.addServlet(new ServletHolder(new FlightByIdServlet()), "/flight/id");
+
+        server.setHandler(handler);
+
+        server.start();
+        server.join();
     }
 }
