63cc829a7d921b55181ce092f48e737de77e37f8
Heybat <heybetmovlamov2005@gmail.com>
2024-07-03 13:14:23 +0100

    FlightService update

--- src/main/java/org/example/service/FlightService.java
+++ src/main/java/org/example/service/FlightService.java
@@ -1,4 +1,31 @@
 package org.example.service;
 
-public class FlightService {
+import org.example.model.dto.FlightDto;
+import org.example.model.entity.FlightEntity;
+import org.example.repository.FlightRepository;
+import org.example.repository.flightDao.FlightDao;
+
+import java.util.Optional;
+
+public class FlightService implements FlightRepository {
+    private final FlightDao flightDao;
+
+    public FlightService(FlightDao flightDao) {
+        this.flightDao = flightDao;
+    }
+
+    @Override
+    public Optional<FlightEntity> getFlightById(int id) {
+        return flightDao.getFlightById(id);
+    }
+
+    @Override
+    public Optional<FlightEntity> getFlightByLocation(String location) {
+        return flightDao.getFlightByLocation(location);
+    }
+
+    @Override
+    public void createFlight(FlightDto flightDto) {
+        flightDao.createFlight(flightDto);
+    }
 }
